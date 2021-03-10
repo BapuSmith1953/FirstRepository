3762aef0309e7c44c841a5f4c54495b23d438bec
BapuSmith1953 <80363262+BapuSmith1953@users.noreply.github.com>
2021-03-10 13:42:04 -0600

    Create SamuelNamestring.py

---
+++ SamuelNamestring.py
@@ -0,0 +1,43 @@
+namestring="SamuelChukwuSmith"
+def delay():
+    for j in range (2000):
+        q= 434345. / 4.555
+        for k in range (3000):
+            junk =3.666577545 /4.66666456
+            for n in range (1000):
+                p = 665432.234 / 4.777
+        return junk
+for i in range (6):
+    modstring = namestring
+    print("*" + modstring + "*")
+    wait = delay()
+    modstring = namestring[1:16]
+    print(" *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[2:15]
+    print("  *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[3:12]
+    print("    *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[5:10]
+    print("      *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[7:8]
+    print("         **")
+    wait = delay()
+    modstring = namestring[5:10]
+    print("      *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[3:12]
+    print("    *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[2:15]
+    print("  *" + modstring + "*")
+    wait = delay()
+    modstring = namestring[1:16]
+    print(" *" + modstring + "*")
+    wait = delay()
+    modstring = namestring
+    print("*" + modstring + "*")
+    wait = delay()
